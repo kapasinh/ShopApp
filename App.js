6ac3dab3111d119ea06bd034f05ecb54360b9dbc
Naman Kapasi <kapasinh@berkeley.edu>
2021-07-05 18:34:08 -0600

    Fix swipe right and left buttons

--- App.js
+++ App.js
@@ -1,21 +1,260 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Dimensions
+} from 'react-native';
+
+import data from './data';
+import Swiper from 'react-native-deck-swiper';
+import { Transitioning, Transition } from 'react-native-reanimated';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const { width } = Dimensions.get('window');
+
+const stackSize = 4;
+const colors = {
+  red: '#EC2379',
+  blue: '#0070FF',
+  gray: '#777777',
+  white: '#ffffff',
+  black: '#000000'
+};
+const ANIMATION_DURATION = 200;
+
+const transition = (
+  <Transition.Sequence>
+    <Transition.Out
+      type='slide-bottom'
+      durationMs={ANIMATION_DURATION}
+      interpolation='easeIn'
+    />
+    <Transition.Together>
+      <Transition.In
+        type='fade'
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+      />
+      <Transition.In
+        type='slide-bottom'
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+        interpolation='easeOut'
+      />
+    </Transition.Together>
+  </Transition.Sequence>
+);
+
+const swiperRef = React.createRef();
+const transitionRef = React.createRef();
+
+const Card = ({ card }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: card.image }} style={styles.cardImage} />
+    </View>
+  );
+};
+
+const CardDetails = ({ index }) => (
+  <View key={data[index].id} style={{ alignItems: 'flex-start' }}>
+    <Text style={[styles.text, styles.heading]} numberOfLines={2}>
+      {data[index].name}
+    </Text>
+    <Text style={[styles.text, styles.price]}>{data[index].price}</Text>
+  </View>
+);
 
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+  const onSwiped = () => {
+    transitionRef.current.animateNextTransition();
+    setIndex((index + 1) % data.length);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MaterialCommunityIcons
+        name='crop-square'
+        size={width}
+        color={colors.blue}
+        style={{
+          opacity: 0.05,
+          transform: [{ rotate: '45deg' }, { scale: 1.6 }],
+          position: 'absolute',
+          left: -15,
+          top: 30
+        }}
+      />
+      <StatusBar hidden={false} />
+      <View style={styles.rightContainer}>
+        <Transitioning.View
+          ref={transitionRef}
+          transition={transition}
+          style={styles.bottomContainerMeta}
+        >
+          <CardDetails index={index} />
+        </Transitioning.View>
+        <View style={styles.bottomContainerButtons}>
+          <MaterialCommunityIcons.Button
+            name='close'
+            size={25}
+            backgroundColor='transparent'
+            underlayColor='transparent'
+            activeOpacity={0.3}
+            color={colors.gray}
+            onPress={() => swiperRef.current.swipeLeft()}
+          />
+        </View>
+      </View>
+
+      <View style={styles.swiperContainer}>
+        <Swiper
+          ref={swiperRef}
+          cards={data}
+          cardIndex={index}
+          renderCard={card => <Card card={card} />}
+          infinite
+          backgroundColor={'transparent'}
+          onSwiped={onSwiped}
+          onTapCard={() => swiperRef.current.swipeLeft()}
+          cardVerticalMargin={0}
+          cardHorizontalMargin={0}
+          stackSize={stackSize}
+          stackScale={10}
+          stackSeparation={14}
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+          disableTopSwipe
+          disableBottomSwipe
+          overlayLabels={{
+            left: {
+              title: 'NOPE',
+              style: {
+                label: {
+                  backgroundColor: colors.red,
+                  borderColor: colors.red,
+                  color: colors.white,
+                  borderWidth: 1,
+                  fontSize: 24
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: -20
+                }
+              }
+            },
+            right: {
+              title: 'LIKE',
+              style: {
+                label: {
+                  backgroundColor: colors.blue,
+                  borderColor: colors.blue,
+                  color: colors.white,
+                  borderWidth: 1,
+                  fontSize: 24
+                },
+                wrapper: {
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: 20
+                }
+              }
+            }
+          }}
+        />
+      </View>
+
+      <View style={styles.leftContainer}>
+        <Transitioning.View
+          ref={transitionRef}
+          transition={transition}
+          style={styles.bottomContainerMeta}
+        >
+          <CardDetails index={index} />
+        </Transitioning.View>
+        <View style={styles.bottomContainerButtons}>
+          <MaterialCommunityIcons.Button
+            name='circle-outline'
+            size={25}
+            backgroundColor='transparent'
+            underlayColor='transparent'
+            activeOpacity={0.3}
+            color={colors.gray}
+            onPress={() => swiperRef.current.swipeRight()}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignContent: 'center',
     justifyContent: 'center',
+    backgroundColor: colors.blue
   },
+  swiperContainer: {
+    flexDirection: 'row',
+    flex: .9,
+    backgroundColor: colors.gray
+  },
+  rightContainer: {
+    flex: .1,
+    justifyContent: 'space-evenly',
+    backgroundColor:colors.red
+  },
+  leftContainer: {
+    flex:.1,
+    justifyContent: 'space-evenly',
+    backgroundColor: colors.blue,
+  },
+  bottomContainerMeta: { alignContent: 'flex-end', alignItems: 'center' },
+  bottomContainerButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  cardImage: {
+    width: 260,
+    flex: .9,
+    resizeMode: 'contain',
+  },
+  card: {
+    flexDirection: 'column',
+    flex: 1,
+    borderRadius: 8,
+    shadowRadius: 40,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white 
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 50,
+    backgroundColor: 'transparent'
+  },
+  done: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: colors.white,
+    backgroundColor: 'transparent'
+  },
+  text: { fontFamily: 'Courier' },
+  heading: { fontSize: 24, marginBottom: 10, color: colors.gray },
+  price: { color: colors.blue, fontSize: 32, fontWeight: '500' }
 });
